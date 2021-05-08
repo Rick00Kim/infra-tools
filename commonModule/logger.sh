77e0a4b37bbd6001a4b1e2448f05029154a9c8bf
Rick00Kim <dreamx119@gmail.com>
2021-05-09 00:47:27 +0900

    Modify sftp tools, common tools

--- commonModule/logger.sh
+++ commonModule/logger.sh
@@ -5,66 +5,8 @@
 # Version : [1.0.0]
 # Writer : Rick_Kim dreamx119@gmail.com
 
-# Define local function
-function connect() {
-   echo "Connect using TNS-> "$1
-   sqlplus $ORACLE_SHEMA@$1
-}
-# Start [Check arguments]
-while getopts "s:t:" option  
-do
-    case $option in
-        s)
-            ORACLE_SHEMA="$OPTARG"
-            ;;
-        t)
-            TNS_FLG="$OPTARG"
-            ;;
-    esac
-done
+# Set Directory name for log
 
-shift $(( $OPTIND -1 ))
 
-# If ORACLE_SHEMA is not exists, exit program.
-if [ -z "$ORACLE_SHEMA" ]; then
-    echo "Oracle shema should not be null"
-    exit 1
-fi
-
-# If TNS_FLG is not setted or is no, get specfic TNS_FLG by user input.
-if [ -z "$TNS_FLG" -o "$TNS_FLG" = "no" ]; then
-    echo "Please write Target TNS sever"
-    read
-    TARGET_TNS=$(echo $REPLY)
-fi
-# End [Check arguments]
-
-# Start [Set variables for script]
-if [ -z $TNS_ADMIN ]; then
-    TNS_DIR=$ORACLE_HOME/network/admin
-else
-    TNS_DIR=$TNS_ADMIN
-fi
-
-# Set Target tns, when TARGET_TNS is not exists.
-if [ -z $TARGET_TNS ]; then
-    # Get all defined TNS.
-    array=(`cat $TNS_DIR/tnsnames.ora | grep "(.*)" | awk -F '=' '{print $1}' | sed 's/ /\n/g'`)
-
-    # Show all TNS.
-    echo "=========TNS Informations=========="
-    for i in "${!array[@]}"
-    do
-        echo "$(($i+1))" -  "${array[$i]}"
-    done
-
-    # Get TNS number by user input.
-    echo "Choose TNS"
-    read
-    TARGET_TNS=${array[$(($REPLY-1))]}
-fi
-
-# Connect to Oracle shema.
-connect $TARGET_TNS
-
+# Set timestamp for log
 
