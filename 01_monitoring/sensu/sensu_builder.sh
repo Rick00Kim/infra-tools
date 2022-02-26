2cdae0460527bd4c8eb61791206d7e5c9b43615e
Rick00Kim <dreamx119@gmail.com>
2022-02-27 04:31:30 +0900

    Modify sensu builder script

--- 01_monitoring/sensu/sensu_builder.sh
+++ 01_monitoring/sensu/sensu_builder.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. common.sh
+
 #######################################################
 # Help                                                #
 #######################################################
@@ -34,7 +36,7 @@ read -p '[Master or Slave]: ' kinds
 if [ -z $kinds ];then
   echo "It will make sensu agent..."
   read -p 'Are you sure?[y/n] - ' default_kind_confirm
-  if [ $default_kind_confirm != "y" ]; then
+  if [ -z $default_kind_confirm ] || [ $default_kind_confirm != "y" ]; then
     exit 1
   else
     kinds="Slave"
@@ -49,10 +51,92 @@ fi
 # Execute Main process
 case $kinds in
   Master)
-    echo "Master start"
+    echo "It starts building sensu go (Master server)"
+    web_ui_port=$(get_port "SENSU WEB UI" 3000)
+    api_port=$(get_port "SENSU API" 8080)
+    agent_port=$(get_port "SENSU AGENT" 8081)
+    admin_user_name=$(get_require_data "Admin Username" "false")
+    admin_password=$(get_require_data "Admin Password" "true")
+
+    check_input \
+      "$web_ui_port" \
+      "$api_port" \
+      "$agent_port" \
+      "$admin_user_name" \
+      "$admin_password"
+
+    if [ $? == 1 ]; then
+      echo "There is wrong input..."
+      exit 1
+    fi
+
+    echo
+    echo "Let we build sensu go with your input below."
+    echo "------------ Input Check -----------------"
+    echo "SENSU WEB UI port: "$web_ui_port
+    echo "SENSU API port: "$api_port
+    echo "SENSU AGENT port: "$agent_port
+    echo "Admin Username: "$admin_user_name
+    echo "Admin Password: "$admin_password
+    echo "------------------------------------------"
+    read -p 'Are you sure?[y/n] - ' confirm_build
+    echo
+    if [ -z $confirm_build ] || [ $confirm_build != "y" ]; then
+      echo "User cancelled or wrong answer..."
+      exit 1
+    else
+      # TODO [Delete exit and comment]
+      exit 1
+      # docker run -v /var/lib/sensu:/var/lib/sensu -d \
+      #     --name sensu-backend \
+      #     -p $web_ui_port:3000 \
+      #     -p $api_port:8080 \
+      #     -p $agent_port:8081 \
+      #     -e SENSU_BACKEND_CLUSTER_ADMIN_USERNAME="$admin_user_name" \
+      #     -e SENSU_BACKEND_CLUSTER_ADMIN_PASSWORD="$admin_password" \
+      #     sensu/sensu:latest \
+      #     sensu-backend start --state-dir /var/lib/sensu/sensu-backend --log-level debug
+      echo "Completed building Sensu go!"
+      echo "ENJOY Sensu monitoring~"
+    fi
     ;;
   Slave)
-    echo "Slave start"
+    echo "It starts building sensu agent (Slave server)"
+    web_socket_url=$(get_require_data "WEB Socket URL" "false")
+    name_space=$(get_require_data "SENSU namespace" "false")
+
+    check_input \
+      "$web_socket_url" \
+      "$name_space"
+
+    if [ $? == 1 ]; then
+      echo "There is wrong input..."
+      exit 1
+    fi
+
+    echo
+    echo "Let we build sensu agent with your input below."
+    echo "------------ Input Check -----------------"
+    echo "WEB Socket URL: "$web_socket_url
+    echo "SENSU namespace: "$name_space
+    echo "------------------------------------------"
+    read -p 'Are you sure?[y/n] - ' confirm_build
+    echo
+    if [ -z $confirm_build ] || [ $confirm_build != "y" ]; then
+      echo "User cancelled or wrong answer..."
+      exit 1
+    else
+      # TODO [Delete exit and comment]
+      exit 1
+      # docker run -v /var/lib/sensu:/var/lib/sensu -d \
+      #   --name sensu-agent sensu/sensu sensu-agent start \
+      #   --name $(uname -n) --namespace $name_space \
+      #   --backend-url ws://$web_socket_url \
+      #   --log-level debug --api-host 0.0.0.0 \
+      #   --cache-dir /var/lib/sensu
+      echo "Completed building Sensu agent!"
+      echo "ENJOY Sensu monitoring~"
+    fi
     ;;
   *)
     echo "A problem has occurred. Please execute script again..."
