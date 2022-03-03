b899612b209e528ab45fd8af8d30f5ce454ff532
Rick00Kim <dreamx119@gmail.com>
2022-03-03 20:33:53 +0900

    Fix grafana builder

--- 01_monitoring/grafana-prometheus/grafana_builder.sh
+++ 01_monitoring/grafana-prometheus/grafana_builder.sh
@@ -25,14 +25,46 @@ if [ -z $confirm_flg ] || [ $confirm_flg != "y" ]; then
 
 # Execute Main process
 # Input
-promethus_port=$(get_port "PROMETHUS UI" 9090)
-container_name=$(get_require_data "PROMETHUS container name" "false")
-api_port=$(get_port "SENSU API" 8080)
-agent_port=$(get_port "SENSU AGENT" 8081)
-admin_password=$(get_require_data "Admin Password" "true")
+promethus_port=$(get_port "PROMETHUS" 9090)
+promethus_container_name=$(get_require_data "PROMETHUS container name" "false")
+grafana_port=$(get_port "GRAFANA" 3000)
+grafana_container_name=$(get_require_data "GRAFANA container name" "false")
 
-docker run -d -p ${promethus_port}:9090 \
-  -v `pwd`/prometheus.yml:/etc/prometheus/prometheus.yml \
-  --name ${container_name} \
-  prom/prometheus
+# Check input information
+check_input \
+  "$promethus_port" \
+  "$promethus_container_name" \
+  "$grafana_port" \
+  "$grafana_container_name"
 
+# When exists wrong input, exit program
+if [ $? == 1 ]; then
+  echo "There is wrong input..."
+  exit 1
+fi
+
+# Show all input
+echo
+echo "Let we build sensu go with your input below."
+echo "------------ Input Check -----------------"
+echo "PROMETHUS port: "$promethus_port
+echo "PROMETHUS container name: "$promethus_container_name
+echo "GRAFANA port: "$grafana_port
+echo "GRAFANA container name: "$grafana_container_name
+echo "------------------------------------------"
+read -p 'Are you sure?[y/n] - ' confirm_build
+echo
+
+# Confirm to user
+if [ -z $confirm_build ] || [ $confirm_build != "y" ]; then
+  echo "User cancelled or wrong answer..."
+  exit 1
+else
+  docker run -d -p ${promethus_port}:9090 \
+    -v `pwd`/prometheus.yml:/etc/prometheus/prometheus.yml \
+    --name ${promethus_container_name} \
+    prom/prometheus
+
+  docker run -d -p ${grafana_port}:3000 \
+    --name=${grafana_container_name} \
+    grafana/grafana
